e472310325a7045837dcf5660cc0ed4087e29d43
madmadhu-1002 <79498740+madmadhu-1002@users.noreply.github.com>
2023-04-24 06:44:25 +0000

    Update form-submit.php

--- form-submit.php
+++ form-submit.php
@@ -1,21 +1,21 @@
 <?php
-// Get the form data
-$date = $_POST['date'];
-$time = $_POST['time'];
-$guests = $_POST['guests'];
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "bookings@restaurant.com";
+    $subject = "Table Booking Request";
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $date = $_POST["date"];
+    $time = $_POST["time"];
+    $guests = $_POST["guests"];
+    $message = "Table booking request details:\n\nDate: $date\nTime: $time\nNumber of guests: $guests\n";
+    $message .= "Guest name: $name\nEmail: $email\nPhone: $phone\n";
+    $headers = "From: $email";
 
-// Send an email to the restaurant's management team
-$to = 'madhumahidhar1002@gmail.com'; //management email
-$subject = 'Table Booking Request';
-$message = "Date: $date\nTime: $time\nNumber of guests: $guests\nName: $name\nPhone: $phone\nEmail: $email";
-$headers = "From: $name <$email>";
-
-if (mail($to, $subject, $message, $headers)) {
-    echo 'Thank you for your reservation. We will contact you shortly to confirm your booking.';
-} else {
-    echo 'Sorry, there was an error processing your reservation request. Please try again later.';
+    if (mail($to, $subject, $message, $headers)) {
+        echo "<p>Thank you for your table booking request. We will be in touch shortly to confirm your booking.</p>";
+    } else {
+        echo "<p>There was a problem submitting your booking request. Please try again later.</p>";
+    }
 }
 ?>
